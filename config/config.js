35a512e37037ebc43c42f41b0c0c78e68a2bd05b
Curt Barnard <luciddr34m3r@gmail.com>
2018-02-08 12:16:46 -0500

    bugfix

--- config/config.js
+++ config/config.js
@@ -17,5 +17,16 @@ module.exports = {
             "description": "The location of the ThreshingFloor API used by your organization.",
             "adminOnly": true
         }
+    ],
+    "block": {
+        "component": {
+            "file": "./component/threshingfloor.js"
+        },
+        "template": {
+            "file": "./templates/ipinfo.hbs"
+        }
+    },
+    "styles": [
+        "./styles/style.less"
     ]
 };
